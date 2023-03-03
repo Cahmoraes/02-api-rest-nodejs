eb992aabc9eb875665eac00f2a323a53f8b80f32
caique.moraes <ike.moraes93@gmail.com>
2023-03-03 17:40:46 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server running at http://localhost:3333')
+  })
